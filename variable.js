3f48e93f879573e05a1352161b101b25cb89532e
myid-hana <58727114+myid-hana@users.noreply.github.com>
2020-06-10 13:41:27 +0900

    Block Scope

--- variable.js
+++ variable.js
@@ -5,3 +5,10 @@
 
 //2. Variable
 //let (added in ES6)
+
+{
+  let name = 'hana';
+  console.log(name); //블럭 내에서는 hana를 출력할 수 있지만
+}
+console.log(name); //블럭 박에서는 hana를 출력할 수 없음. 아무값도 출력되지 않는다.
+//Block scope: 블럭 밖에서는 블럭 안에 있는 내용에 접근할 수 없다.
