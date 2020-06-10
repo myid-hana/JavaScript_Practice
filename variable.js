7c8e2be423a6cd0e1a90f35eeaca7ad1ecd7580d
myid-hana <58727114+myid-hana@users.noreply.github.com>
2020-06-10 15:38:33 +0900

    boolean, null, undefined, symbol

--- variable.js
+++ variable.js
@@ -55,3 +55,26 @@ const helloBob = `hi ${brendan}!`;
 console.log(`value: ${helloBob}, type: ${typeof helloBob}`);  //출력값: value: hi brendan!, type: string
 console.log('value: ' + helloBob + ' type: ' + typeof helloBob);
 //template literals를 쓰면 복잡하게 플러스로 string과 변수를 연결하던 것들을 간편하게 쓸 수 있다.
+
+//boolean
+//false: 0, null, undefined, NaN, '' 모두 false로 간주
+//true: any other value
+const canRead = true;
+const test = 3 < 1; //false
+
+//null
+let nothing = null;
+//null로 빈 값이 할당되어 있는 상태
+
+let x;
+//undefined는 아무 값도 할당되지 않은 상태
+
+//symbol 맵이나 자료구조에서 고유한 식별자가 필요할 때, 동시 다발적으로 일어나는 코드에서 우선순위를 주기 위해서 정말 고유한 식별자가 필요할 때 사용한다. 식별자를 string으로 쓰게 되면 다른 파일에서  같은 string을 썼을 때 동일한 식별자로 간주되지만, symbol은 동일한 string으로 만들어도 다르게 간주되기 때문에 string에 상관없이 고유한 식별자를 만들 때 사용되어 진다.
+const symbol1 = Symbol('id');
+const symbol2 = Symbol('id');
+console.log(symbol1 === symbol2); //false
+const gSymbol1 = Symbol.for('id');
+const gSymbol2 = Symbol.for('id');
+console.log(gSymbol1 === gSymbol2); //true. Symbol.for을 이용하면 동일한 symbol을 만들 수 있다.
+console.log(`value: ${symbol1}, type: ${typeof symbol1}`); //error. symbol은 바로 출력하면 에러가 발생한다.
+console.log(`value: ${symbol1.description}, type: ${typeof symbol1}`);  //.description을 이용해서 string으로 출력해줘야 한다. 
