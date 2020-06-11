6913d95f4db9e96a048ab84e2a1335b2f5aa805e
myid-hana <58727114+myid-hana@users.noreply.github.com>
2020-06-11 18:04:09 +0900

    equality

--- operator.js
+++ operator.js
@@ -14,4 +14,13 @@ x /= y;
 //3. Logical operators: ||(or), &&(and), !(not)
 //||(or)는 제시된 값 중 어느 하나라도 일단 true가 나오면 뒷부분 값이 true인지 false인지 상관없이 true가 되기 때문에 더이상 함수를 진행시키지 않고 다음으로 넘어간다.
 //그래서 심플한 값을 먼저 배치하고 무거운 함수를 뒤쪽에 배치하는 것이 효율적이다.
-//&&(and)도 마찬가지로 일단 false값이 나오면 뒷부분값과 상관없이 무조건 false기 때문에 심플한 값을 먼저 배치하는 것이 좋다. 
+//&&(and)도 마찬가지로 일단 false값이 나오면 뒷부분값과 상관없이 무조건 false기 때문에 심플한 값을 먼저 배치하는 것이 좋다.
+
+
+//equality
+console.log(0 == false);  //true
+console.log(0 === false); //false
+console.log('' == false); //true
+console.log('' === false); //false
+console.log(null == undefined); //true
+console.log(null === undefined); //false
