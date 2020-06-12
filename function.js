5091bb5b7037938a7fa664faed861da0a9a6c899
myid-hana <58727114+myid-hana@users.noreply.github.com>
2020-06-12 17:02:54 +0900

    함수 선언

---
+++ function.js
@@ -0,0 +1,10 @@
+//1. Use strict
+//added in ES 5
+//Use this for Vanilla JavaScript
+'use strict';
+
+
+//1. 함수 선언
+//하나의 함수는 한 가지의 일만 하도록 만들어야 한다.
+//동사 형태로 이름을 짓는 것이 좋다.
+//js에서 function은 object이다.
