1f2de7384f88508f16a12043cd328fd77f05e863
myid-hana <58727114+myid-hana@users.noreply.github.com>
2020-06-14 17:37:37 +0900

    getter and setter

--- class.js
+++ class.js
@@ -7,7 +7,7 @@
 //object: instance of a class, created many times, data in
 
 
-//class declaration
+//1. class declaration
 class Person {
   //constructor 생성자
   constructor(name, age) {
@@ -26,3 +26,24 @@ const hana = new Person('hana', 30); //object 생성
 console.log(hana.name);
 console.log(hana.age);
 hana.speak();
+
+
+//2. getter and setters
+class User {
+  constructor(firstName, lastName, age) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
+
+  get age() {//사용자가 잘못 설정하는 것을 방지
+    return this._age; //다른 이름의 변수를 한 개 더 만들어줌.
+  }
+
+  set age(value) {
+    this._age = value < 0 ? 0 : value;  //value의 값이 0이하 라면 0으로 하고 아니면 지정된 value값을 사용.
+  }
+}
+
+const user1 = new User('hana', 'job', -1);
+console.log(user1.age);
